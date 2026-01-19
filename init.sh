dd63e4172d4d518a5daf9e3fc71b5cb7a82f01e8
Collin Stevens <git@collinstevens.com>
2026-01-19 01:02:56 -0800

    add .bashrc

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 links=(
+    ".bashrc:$HOME/.bashrc"
     ".gitconfig:$HOME/.gitconfig"
     ".gitconfig-linux:$HOME/.gitconfig-linux"
 )
